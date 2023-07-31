fc12485fcfb8e338158c7f35b7048b934eed3daa
Florian Moser <git@famoser.ch>
2023-07-31 21:09:10 +0200

    maint: Remove unused

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -33,12 +33,7 @@ export function activate(context: ExtensionContext) {
 
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
 		documentSelector: [{ scheme: 'file', language: 'pv' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
 	};
 
 	// Create the language client and start the client.

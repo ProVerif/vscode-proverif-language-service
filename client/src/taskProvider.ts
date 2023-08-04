64169f64330ae2ecbfc7c6be36839152b58dd42f
Florian Moser <git@famoser.ch>
2023-08-04 22:08:55 +0200

    fix: Ensure ProVerif build only appears on ProVerif files

--- client/src/taskProvider.ts
+++ client/src/taskProvider.ts
@@ -19,19 +19,23 @@ class ProVerifTaskProvider implements vscode.TaskProvider {
 	static CustomBuildScriptType = 'ProVerif';
 
 	public async provideTasks(): Promise<vscode.Task[]> {
-		return [this.getBuildTask()];
+		const currentUri = vscode.window.activeTextEditor?.document.uri;
+		if (!currentUri.path.endsWith('.pv')) {
+			return [];
+		}
+		return [this.getBuildTask(currentUri)];
 	}
 
 	public resolveTask(_task: vscode.Task): vscode.Task | undefined {
 		return undefined;
 	}
 
-	private getBuildTask(): vscode.Task {
-		const currentFilepath = vscode.window.activeTextEditor?.document.uri.fsPath;
+	private getBuildTask(uri: vscode.Uri): vscode.Task {
+		const currentFilepath = uri.fsPath;
 		const content = readFileSync(currentFilepath).toString();
 		const libraryDependencies = parseLibraryDependenciesNoValidate(currentFilepath, content);
 
-		const settings = vscode.workspace.getConfiguration('proverif', vscode.window.activeTextEditor?.document.uri);
+		const settings = vscode.workspace.getConfiguration('proverif', uri);
 		const proverifBinary = settings.proverifPath ? settings.proverifPath : 'proverif';
 
 		const task = new vscode.Task({ type: ProVerifTaskProvider.CustomBuildScriptType }, vscode.TaskScope.Workspace, 'Build', ProVerifTaskProvider.CustomBuildScriptType, new vscode.ShellExecution(`${proverifBinary} ${libraryDependencies} ${currentFilepath}`));

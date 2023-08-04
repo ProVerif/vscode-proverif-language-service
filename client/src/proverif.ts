00f2545334b21e0f87f45d830eebb59e006e8c6c
Florian Moser <git@famoser.ch>
2023-08-04 21:48:37 +0200

    fix: Ensure libraries only included once in build task

--- client/src/proverif.ts
+++ client/src/proverif.ts
@@ -4,14 +4,14 @@ const LIB_ARGUMENT_PREFIX = '-lib ';
 const LIB_FILE_ENDING = '.pvl';
 const LIB_REGEX_MATCH = /\(\* +-lib (.+)\.pvl/g;
 export const parseLibraryDependenciesNoValidate = (filePath: string, content: string) => {
-    const expectedLocations = [];
+    const expectedLocations = new Set();
     
     const folder = filePath.split(sep).slice(0, -1).join(sep);
     const matches = content.matchAll(LIB_REGEX_MATCH);
     for (const match of matches) {
         const expectedFilename = match[1] + LIB_FILE_ENDING;
-        expectedLocations.push(folder + sep + expectedFilename);
+        expectedLocations.add(folder + sep + expectedFilename);
     }
 
-    return expectedLocations.map(expectedLocation => LIB_ARGUMENT_PREFIX + expectedLocation).join(" ");
+    return Array.from(expectedLocations).map(expectedLocation => LIB_ARGUMENT_PREFIX + expectedLocation).join(" ");
 };

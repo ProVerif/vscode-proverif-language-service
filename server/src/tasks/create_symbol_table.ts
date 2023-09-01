5bedfcf3ce9a2e61e0b6c34384e5054ef5913f34
Florian Moser <git@famoser.ch>
2023-09-01 20:40:26 +0200

    refactor: Drop antlr-c3 symbol table as mostly undocumented

--- server/src/tasks/create_symbol_table.ts
+++ server/src/tasks/create_symbol_table.ts
@@ -1,73 +1,152 @@
 import {AbstractParseTreeVisitor, ParseTree} from "antlr4ts/tree";
-import {ScopedSymbol, SymbolTable, VariableSymbol} from "antlr4-c3";
 import {ProverifParserVisitor} from "../parser-proverif/ProverifParserVisitor";
-import {DocumentUri} from "vscode-languageserver-textdocument";
-import {ChannelDeclarationContext, IdentifierContext, ProverifFileContext} from "../parser-proverif/ProverifParser";
-import {LocationLink, TextDocumentIdentifier} from "vscode-languageserver";
-import {getRange} from "../parseTree/get_range";
+import {
+    LibContext,
+    NeidentseqContext,
+    NevartypeContext,
+    OnevartypeContext,
+    TprocessContext
+} from "../parser-proverif/ProverifParser";
+import {TerminalNode} from "antlr4ts/tree/TerminalNode";
+
 
 export type CreateSymbolTableResult = {
-    symbolTable: SymbolTable
+    symbolTable: ProverifSymbolTable
 }
 
-export const createSymbolTable = (identifier: TextDocumentIdentifier, context: ProverifFileContext): CreateSymbolTableResult => {
-    const symbolTableVisitor = new SymbolTableVisitor(identifier.uri);
-    return { symbolTable: symbolTableVisitor.visit(context) };
+export const createSymbolTable = (context: ParseTree): CreateSymbolTableResult => {
+    const symbolTableVisitor = new SymbolTableVisitor();
+    return {symbolTable: symbolTableVisitor.visit(context)};
 };
 
-class SymbolTableVisitor extends AbstractParseTreeVisitor<SymbolTable> implements ProverifParserVisitor<SymbolTable> {
+class SymbolTableVisitor extends AbstractParseTreeVisitor<ProverifSymbolTable> implements ProverifParserVisitor<ProverifSymbolTable> {
     constructor(
-        public documentUri: DocumentUri,
-        protected readonly symbolTable = new SymbolTable("", {}),
-        protected scope = symbolTable.addNewSymbolOfType(ScopedSymbol, undefined)) {
+        private readonly symbolTable = new ProverifSymbolTable(),
+        private context: undefined | ParseTree = undefined) {
         super();
     }
 
-    protected defaultResult(): SymbolTable {
+    protected defaultResult(): ProverifSymbolTable {
         return this.symbolTable;
     }
 
-    visitProverifFile = (ctx: ProverifFileContext) => {
-        this.scope.context = ctx;
+    private collectNeidentseqIDENTs = (ctx?: NeidentseqContext): TerminalNode[] => {
+        if (!ctx) {
+            return [];
+        }
+
+        return [ctx.IDENT(), ...this.collectNeidentseqIDENTs(ctx.neidentseq())];
+    };
+
+    private collectNevartypeIDENTs = (ctx?: NevartypeContext): TerminalNode[] => {
+        if (!ctx) {
+            return [];
+        }
+
+        return [
+            ...this.collectOnevartypeIDENTs(ctx.onevartype()),
+            ...this.collectNevartypeIDENTs(ctx.nevartype())
+        ];
+    };
+
+    private collectOnevartypeIDENTs = (ctx?: OnevartypeContext): TerminalNode[] => {
+        if (!ctx) {
+            return [];
+        }
+
+        return [ctx.IDENT(), ...this.collectNeidentseqIDENTs(ctx.neidentseq())];
+    };
+
+    public visitLib = (ctx: LibContext) => {
+        const identifierList = ctx.neidentseq();
+        if (identifierList && (ctx.CONST() || ctx.CHANNEL() || ctx.FREE())) {
+            this.collectNeidentseqIDENTs(identifierList).forEach(identifier => {
+                this.registerVariable(identifier);
+            });
+        }
+
+        const identifier = ctx.IDENT();
+        if (identifier && (ctx.TYPE() || ctx.FUN() || ctx.EVENT() || ctx.PREDICATE() || ctx.TABLE() || ctx.LET() || ctx.LETFUN())) {
+            this.registerVariable(identifier);
+        }
+
         return this.visitChildren(ctx);
     };
 
-    visitChannelDeclaration = (ctx: ChannelDeclarationContext) => {
-        ctx.identifierSequence()?.children?.forEach(identifier => {
-            if (identifier instanceof IdentifierContext) {
-                const symbol = this.symbolTable.addNewSymbolOfType(VariableSymbol, this.scope, identifier.text, undefined);
-                this.registerDefinition(symbol, identifier, identifier.Identifier());
+    public visitTprocess = (ctx: TprocessContext) => {
+        return this.withContext(ctx, () => {
+            const tpattern = ctx.tpattern();
+            if (tpattern && (ctx.IN() || ctx.LET())) {
+                // TODO parse patterns
             }
-        });
 
-        return this.defaultResult();
+            const nevartype = ctx.nevartype();
+            if (tpattern && (ctx.LET())) {
+                this.collectNevartypeIDENTs(nevartype).forEach(identifier => {
+                    this.registerVariable(identifier);
+                });
+            }
+
+            const identifier = ctx.IDENT();
+            if (identifier.length === 1 && (ctx.NEW() && ctx.RANDOM())) {
+                this.registerVariable(identifier[0]);
+            }
+
+            const basicPattern = ctx.basicpattern();
+            const basicPatternIdent = basicPattern?.IDENT();
+            if (basicPattern && (ctx.LEFTARROW() && basicPatternIdent)) {
+                this.registerVariable(basicPatternIdent);
+            }
+
+            return this.visitChildren(ctx);
+        });
     };
 
-    protected registerDefinition(symbol: any, tree: ParseTree, definitionName: ParseTree) {
-        const targetRange = getRange(tree);
-        const targetSelectionRange = getRange(definitionName);
-
-        if (targetRange && targetSelectionRange) {
-            symbol.location = LocationLink.create(this.documentUri, targetRange, targetSelectionRange);
-        }
+    private registerVariable(identifier: TerminalNode) {
+        this.symbolTable.addSymbol(identifier, this.context);
     }
 
     /**
      visitFunctionDeclaration = (ctx: FunctionDeclarationContext) => {
      return this.withScope(ctx, RoutineSymbol, [ctx.identifier().text], () => this.visitChildren(ctx));
      };
+     */
 
-     protected withScope<T>(tree: ParseTree, type: new (...args: any[]) => ScopedSymbol, args: any[], action: () => T): T {
-     const scope = this.symbolTable.addNewSymbolOfType(type, this.scope, ...args);
-     scope.context = tree;
-     this.scope = scope;
-     try {
-     this.registerDefinition(this.scope, definition, definitionName);
-     return action();
-     } finally {
-     this.scope = scope.parent as ScopedSymbol;
-     }
-     }
-     **/
+    private withContext<T>(context: ParseTree, action: () => T): T {
+        this.context = context;
+        try {
+            return action();
+        } finally {
+            this.context = context.parent;
+        }
+    }
+}
+
+
+type ProverifSymbol = {
+    node: TerminalNode
+    context?: ParseTree
+}
+
+class ProverifSymbolTable {
+    private symbols: ProverifSymbol[] = [];
+
+    public addSymbol(node: TerminalNode, context?: ParseTree) {
+        this.symbols.push({node, context});
+    }
+
+    public findClosestSymbol(node: ParseTree): ParseTree | undefined {
+        return this.findClosestSymbolInternal(node.text, node);
+    }
+
+    private findClosestSymbolInternal(name: string, context?: ParseTree): ParseTree | undefined {
+        const symbolsOfContext = this.symbols.filter(symbol => symbol.context === context);
+        const matchingSymbol = symbolsOfContext.find(symbol => symbol.node.text === name);
+        if (matchingSymbol) {
+            return matchingSymbol.node;
+        }
+
+        return context ? this.findClosestSymbolInternal(name, context.parent) : undefined;
+    }
 }
 

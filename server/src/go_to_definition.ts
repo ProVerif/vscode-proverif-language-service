cc7c78ec519b1f2a06e0c36c70f9ed48259337c3
Florian Moser <git@famoser.ch>
2023-09-08 11:59:47 +0200

    maint: Remove unused definition

--- server/src/go_to_definition.ts
+++ server/src/go_to_definition.ts
@@ -4,7 +4,6 @@ import {getTokenPosition} from "./parseTree/compute_token_position";
 import {getRange} from "./parseTree/get_range";
 import {DefinitionLink} from "vscode-languageserver-protocol";
 import {ParseTree} from "antlr4ts/tree";
-import {Range} from "vscode-languageserver/node";
 
 export const getDefinitionLink = async (uri: TextDocumentIdentifier, parseResult: ParseResult, position: Position): Promise<DefinitionLink | undefined> => {
     const caretPosition = {line: position.line + 1, column: position.character};

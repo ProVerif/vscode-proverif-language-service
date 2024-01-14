8d8c22ce87e47ed960ded84aa5178493ca146861
Florian Moser <git@famoser.ch>
2024-01-14 21:45:17 +0100

    maint: Code style / comments

--- server/src/rename.ts
+++ server/src/rename.ts
@@ -3,5 +3,5 @@ import {ParseResult} from "./document_manager";
 import {WorkspaceEdit} from "vscode-languageserver-protocol";
 
 export const rename = async (uri: TextDocumentIdentifier, parseResult: ParseResult, position: Position, newName: string): Promise<WorkspaceEdit | undefined> => {
-    return undefined
-}
+    return undefined;
+};

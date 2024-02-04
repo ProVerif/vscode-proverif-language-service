9dd8ab6ea48d5679105f8adb567e3a189dac7386
Florian Moser <git@famoser.ch>
2024-02-04 13:01:12 +0100

    refactor: Refactor get matching parse tree

--- server/src/go_to_definition.ts
+++ server/src/go_to_definition.ts
@@ -1,6 +1,6 @@
 import {ParseResult} from "./document_manager";
 import {DocumentLink, LocationLink, Position, TextDocumentIdentifier} from "vscode-languageserver";
-import {getTokenPosition, TokenPosition} from "./parseTree/get_token_position";
+import {getMatchingParseTree} from "./parseTree/get_matching_parse_tree";
 import {getRange} from "./parseTree/get_range";
 import {DefinitionLink} from "vscode-languageserver-protocol";
 import {ParseTree} from "antlr4ts/tree";
@@ -14,14 +14,14 @@ export const getDocumentLinks = async (parseResult: ParseResult): Promise<Docume
 
 export type DefinitionSymbol = ProverifSymbol & { uri: TextDocumentIdentifier }
 
-export const getDefinitionSymbol = async (uri: TextDocumentIdentifier, parseResult: ParseResult, tokenPosition: TokenPosition): Promise<DefinitionSymbol | undefined> => {
-    const closestSymbol = parseResult.symbolTable.findClosestSymbol(tokenPosition.context);
+export const getDefinitionSymbol = async (uri: TextDocumentIdentifier, parseResult: ParseResult, matchingParseTree: ParseTree): Promise<DefinitionSymbol | undefined> => {
+    const closestSymbol = parseResult.symbolTable.findClosestSymbol(matchingParseTree);
     if (closestSymbol) {
         return {...closestSymbol, uri};
     }
 
     for (let i = 0; i < parseResult.dependencies.length; i++) {
-        const closestSymbol = parseResult.dependencies[i].symbolTable.findClosestSymbol(tokenPosition.context);
+        const closestSymbol = parseResult.dependencies[i].symbolTable.findClosestSymbol(matchingParseTree);
         if (!closestSymbol) {
             continue;
         }
@@ -33,17 +33,17 @@ export const getDefinitionSymbol = async (uri: TextDocumentIdentifier, parseResu
 };
 
 export const getDefinitionLink = async (uri: TextDocumentIdentifier, parseResult: ParseResult, position: Position): Promise<DefinitionLink | undefined> => {
-    const tokenPosition = getTokenPosition(parseResult.parserTree, parseResult.parser.inputStream, position);
-    if (!tokenPosition) {
+    const matchingParseTree = getMatchingParseTree(parseResult.parserTree, parseResult.parser.inputStream, position);
+    if (!matchingParseTree) {
         return undefined;
     }
 
-    const definitionSymbol = await getDefinitionSymbol(uri, parseResult, tokenPosition);
+    const definitionSymbol = await getDefinitionSymbol(uri, parseResult, matchingParseTree);
     if (!definitionSymbol) {
         return undefined;
     }
 
-    return constructLocationLink(definitionSymbol.uri, definitionSymbol.node, tokenPosition.context);
+    return constructLocationLink(definitionSymbol.uri, definitionSymbol.node, matchingParseTree);
 };
 
 const constructLocationLink = (identifier: TextDocumentIdentifier, target: ParseTree, source: ParseTree): LocationLink | undefined => {
---
+++ server/src/parseTree/get_matching_parse_tree.ts
@@ -0,0 +1,53 @@
+import {ParseTree, TerminalNode} from "antlr4ts/tree";
+import {ParserRuleContext, Token, TokenStream} from "antlr4ts";
+import {Position} from "vscode-languageserver";
+
+export type CaretPosition = { line: number, column: number };
+
+export const getMatchingParseTree = (parseTree: ParseTree, tokens: TokenStream, position: Position): ParseTree | undefined => {
+    const caretPosition = {line: position.line + 1, column: position.character};
+    return getMatchingParseTreeInternal(parseTree, tokens, caretPosition);
+};
+
+const getMatchingParseTreeInternal = (parseTree: ParseTree, tokens: TokenStream, caretPosition: CaretPosition): ParseTree | undefined => {
+    if (parseTree instanceof TerminalNode) {
+        const token = parseTree.symbol;
+        if (checkCaretPositionOverlapsToken(caretPosition, token)) {
+            return parseTree;
+        }
+    }
+
+    if (parseTree instanceof ParserRuleContext) {
+        // early out
+        if ((parseTree.start.line > caretPosition.line) ||
+            (parseTree.stop && parseTree.stop.line < caretPosition.line)) {
+            return undefined;
+        }
+
+        // check if children match
+        for (let i = 0; i < parseTree.childCount; i++) {
+            const matchingParseTree = getMatchingParseTreeInternal(parseTree.getChild(i), tokens, caretPosition);
+            if (matchingParseTree) {
+                return matchingParseTree;
+            }
+        }
+
+        // else, check if self matches. ignore not finished parsed trees
+        let maxTokenIndex = parseTree.stop ? parseTree.stop?.tokenIndex + 1 : 0;
+        for (let i = parseTree.start.tokenIndex; i < maxTokenIndex; i++) {
+            if (checkCaretPositionOverlapsToken(caretPosition, tokens.get(i))) {
+                return parseTree;
+            }
+        }
+    }
+
+    return undefined;
+};
+
+const checkCaretPositionOverlapsToken = (caretPosition: CaretPosition, token: Token) => {
+    return token.text &&
+        token.line === caretPosition.line &&
+        token.charPositionInLine <= caretPosition.column &&
+        token.charPositionInLine + (token.text?.length ?? 0) >= caretPosition.column;
+
+};

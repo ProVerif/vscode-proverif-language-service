c005d154c200114cd1cf7f40871033e8e243843d
Florian Moser <git@famoser.ch>
2024-02-04 15:56:01 +0100

    refactor: Expose library tokens

--- server/src/go_to_definition.ts
+++ server/src/go_to_definition.ts
@@ -1,44 +1,48 @@
-import {ParseResult} from "./document_manager";
-import {DocumentLink, LocationLink, Position, TextDocumentIdentifier} from "vscode-languageserver";
+import {DocumentManager, ParseResult} from "./document_manager";
+import {LocationLink, Position, TextDocumentIdentifier} from "vscode-languageserver";
 import {getMatchingParseTree} from "./parseTree/get_matching_parse_tree";
 import {getRange} from "./parseTree/get_range";
 import {DefinitionLink} from "vscode-languageserver-protocol";
 import {ParseTree} from "antlr4ts/tree";
 import {ProverifSymbol} from "./tasks/create_symbol_table";
 
-export const getDocumentLinks = async (parseResult: ParseResult): Promise<DocumentLink[]> => {
-    return parseResult.dependencies
-        .filter(dependency => dependency.exists)
-        .map(dependency => DocumentLink.create(dependency.range, dependency.uri));
-};
-
 export type DefinitionSymbol = ProverifSymbol & { uri: TextDocumentIdentifier }
 
-export const getDefinitionSymbol = async (parseResult: ParseResult, matchingParseTree: ParseTree): Promise<DefinitionSymbol | undefined> => {
+export const getDefinitionSymbol = async (parseResult: ParseResult, matchingParseTree: ParseTree, documentManager: DocumentManager): Promise<DefinitionSymbol | undefined> => {
     const closestSymbol = parseResult.symbolTable.findClosestSymbol(matchingParseTree);
     if (closestSymbol) {
         return {...closestSymbol, uri: parseResult.identifier};
     }
 
-    for (let i = 0; i < parseResult.dependencies.length; i++) {
-        const closestSymbol = parseResult.dependencies[i].symbolTable.findClosestSymbol(matchingParseTree);
+    for (let i = 0; i < parseResult.dependencyTokens.length; i++) {
+        const dependency = parseResult.dependencyTokens[i]
+        if (!dependency.exists) {
+            continue
+        }
+
+        const dependencyParseResult = await documentManager.getParseResult(dependency)
+        if (!dependencyParseResult) {
+            continue;
+        }
+
+        const closestSymbol = dependencyParseResult.symbolTable.findClosestSymbol(matchingParseTree);
         if (!closestSymbol) {
             continue;
         }
 
-        return {...closestSymbol, uri: parseResult.dependencies[i]};
+        return {...closestSymbol, uri: dependency};
     }
 
     return undefined;
 };
 
-export const getDefinitionLink = async (parseResult: ParseResult, position: Position): Promise<DefinitionLink | undefined> => {
+export const getDefinitionLink = async (parseResult: ParseResult, position: Position, documentManager: DocumentManager): Promise<DefinitionLink | undefined> => {
     const matchingParseTree = getMatchingParseTree(parseResult.parserTree, parseResult.parser.inputStream, position);
     if (!matchingParseTree) {
         return undefined;
     }
 
-    const definitionSymbol = await getDefinitionSymbol(parseResult, matchingParseTree);
+    const definitionSymbol = await getDefinitionSymbol(parseResult, matchingParseTree, documentManager);
     if (!definitionSymbol) {
         return undefined;
     }

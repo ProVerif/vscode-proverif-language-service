594fe31135fab77dddbbd807d63f3193a7a22510
Florian Moser <git@famoser.ch>
2024-02-08 20:09:43 +0100

    feat: Add rename support

--- server/src/rename.ts
+++ server/src/rename.ts
@@ -1,8 +1,25 @@
 import {Position, TextDocumentIdentifier} from "vscode-languageserver";
 import {DocumentManagerInterface} from "./document_manager";
 import {WorkspaceEdit} from "vscode-languageserver-protocol";
+import {getReferences} from "./references";
+import {DocumentUri, TextEdit} from "vscode-languageserver-types";
 
-export const rename = async (uri: TextDocumentIdentifier, position: Position, newName: string, documentManager: DocumentManagerInterface): Promise<WorkspaceEdit | undefined> => {
-    // TODO implement. Use concept of references.
-    return undefined;
+export const rename = async (identifier: TextDocumentIdentifier, position: Position, newText: string, documentManager: DocumentManagerInterface): Promise<WorkspaceEdit | undefined> => {
+    const references = await getReferences(identifier, position, documentManager)
+    if (!references) {
+        return undefined
+    }
+
+    const changes: { [uri: DocumentUri]: TextEdit[] } = {}
+    references.forEach(reference => {
+        if (!changes[reference.uri]) {
+            changes[reference.uri] = []
+        }
+
+        changes[reference.uri].push({range: reference.range, newText: newText})
+    })
+
+    console.log(changes, references)
+
+    return {changes}
 };

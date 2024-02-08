2965d20258c31fd5edfefeaf896cd0ec73fdd874
Florian Moser <git@famoser.ch>
2024-02-08 19:32:28 +0100

    feat: Add discovery of dependencies in parent folders

--- server/src/utils/settings.ts
+++ server/src/utils/settings.ts
@@ -3,13 +3,15 @@ import {TextDocumentIdentifier} from "vscode-languageserver";
 
 export type ProVerifSettings = {
     proverifBinary: string;
+    parentFolderDiscoveryLimit: number
 }
 
 type RawProVerifSettings = {
     proverifPath?: string;
+    parentFolderDiscoveryLimit: number
 }
 
-const defaultSettings: ProVerifSettings = {proverifBinary: 'proverif'};
+const defaultSettings: ProVerifSettings = {proverifBinary: 'proverif', parentFolderDiscoveryLimit: 1};
 export const getDocumentSettings = async (connection: Connection, hasConfigurationCapability: boolean, document: TextDocumentIdentifier): Promise<ProVerifSettings> => {
     if (!hasConfigurationCapability) {
         return defaultSettings;
@@ -21,6 +23,7 @@ export const getDocumentSettings = async (connection: Connection, hasConfigurati
     }) as RawProVerifSettings;
 
     return {
-        proverifBinary: config.proverifPath ?? defaultSettings.proverifBinary
+        proverifBinary: config.proverifPath ?? defaultSettings.proverifBinary,
+        parentFolderDiscoveryLimit: config.parentFolderDiscoveryLimit
     };
 };

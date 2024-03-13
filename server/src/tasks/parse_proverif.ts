7e339ff82736e6ef2bd8e680ac34c7ba0d0cbb4f
Florian Moser <git@famoser.ch>
2024-03-13 11:31:58 +0100

    fix: Prevent crashes for invalid grammars

--- server/src/tasks/parse_proverif.ts
+++ server/src/tasks/parse_proverif.ts
@@ -1,19 +1,26 @@
 import {CharStreams, CommonTokenStream} from "antlr4ts";
-import {ProverifParser} from '../parser-proverif/ProverifParser';
+import {AllContext, LibContext, ProverifParser} from '../parser-proverif/ProverifParser';
 import {ProverifLexer} from "../parser-proverif/ProverifLexer";
 import {ParseTree} from "antlr4ts/tree";
 
 export type ParseProverifResult = {
     parser: ProverifParser
-    parserTree: ParseTree
+    parserTree: LibContext|AllContext
 }
 
-export const parseProverif = (content: string, selfIsLibrary: boolean) => {
-    const input = CharStreams.fromString(content);
-    const lexer = new ProverifLexer(input);
-    const commonTokenStream = new CommonTokenStream(lexer);
-    const parser = new ProverifParser(commonTokenStream);
-    const parserTree = selfIsLibrary ? parser.lib() : parser.all();
-
-    return { parser, parserTree };
+export const parseProverif = <Context extends boolean>(content: string, selfIsLibrary: Context): {
+    parser: ProverifParser,
+    parserTree: (Context extends true ? LibContext : AllContext)
+} | undefined => {
+    try {
+        const input = CharStreams.fromString(content);
+        const lexer = new ProverifLexer(input);
+        const commonTokenStream = new CommonTokenStream(lexer);
+        const parser = new ProverifParser(commonTokenStream);
+        const parserTree = (selfIsLibrary ? parser.lib() : parser.all()) as Context extends true ? LibContext : AllContext;
+        return {parser, parserTree};
+    } catch (e) {
+        // parsing might fail because there is a syntax error in code
+        return undefined;
+    }
 };

786b040753d6b2ea84bb4b80234928d8a8e98c58
Florian Moser <git@famoser.ch>
2024-03-25 07:42:06 +0100

    refactor: Avoid recursing twice

--- server/src/go_to_definition.ts
+++ server/src/go_to_definition.ts
@@ -4,8 +4,9 @@ import {getMatchingParseTree} from "./parseTree/get_matching_parse_tree";
 import {getRange} from "./parseTree/get_range";
 import {DefinitionLink} from "vscode-languageserver-protocol";
 import {ParseTree} from "antlr4ts/tree";
-import {ProverifSymbol} from "./tasks/create_symbol_table";
+import {DeclarationType, ProverifSymbol} from "./tasks/create_symbol_table";
 import {TerminalNode} from "antlr4ts/tree/TerminalNode";
+import {LibraryDependencyToken} from "./tasks/parse_library_dependencies";
 
 type Origin = { uri: TextDocumentIdentifier, match: TerminalNode };
 export type DefinitionSymbol = { uri: TextDocumentIdentifier, symbol: ProverifSymbol, origin: Origin }
@@ -26,22 +27,27 @@ export const getDefinitionSymbolFromPosition = async (identifier: TextDocumentId
 
 
 export const getDefinitionSymbolFromMatch = async (parseResult: ParseResult, matchingParseTree: TerminalNode, documentManager: DocumentManagerInterface): Promise<DefinitionSymbol | undefined> => {
-    const definition = getDefinitionSymbolFromMatchMacroAware(parseResult, matchingParseTree, documentManager, false);
-    if (definition) {
-        return definition;
-    }
-
-    return getDefinitionSymbolFromMatchMacroAware(parseResult, matchingParseTree, documentManager, true);
+    return  getDefinitionSymbolFromMatchMacroAware(parseResult, matchingParseTree, documentManager);
 };
 
-export const getDefinitionSymbolFromMatchMacroAware = async (parseResult: ParseResult, matchingParseTree: TerminalNode, documentManager: DocumentManagerInterface, considerMacros: boolean): Promise<DefinitionSymbol | undefined> => {
+const getDefinitionSymbolFromMatchMacroAware = async (parseResult: ParseResult, matchingParseTree: TerminalNode, documentManager: DocumentManagerInterface): Promise<DefinitionSymbol | undefined> => {
     const origin = {uri: parseResult.identifier, match: matchingParseTree};
 
-    const closestSymbol = parseResult.symbolTable.findClosestSymbol(matchingParseTree.text, matchingParseTree, considerMacros);
+    const closestSymbol = parseResult.symbolTable.findClosestSymbol(matchingParseTree.text, matchingParseTree);
     if (closestSymbol) {
+        if (closestSymbol.declaration === DeclarationType.ExpandParameter) {
+            // TODO: break the cycle
+            const originSymbol = await getDefinitionSymbolFromMatch(parseResult, closestSymbol.node, documentManager);
+            if (originSymbol) {
+                return originSymbol;
+            }
+        }
+
         return {uri: parseResult.identifier, symbol: closestSymbol, origin};
     }
 
+    let currentClosestSymbol: ProverifSymbol|undefined;
+    let currentClosestSymbolDependency: LibraryDependencyToken|undefined;
     for (let i = 0; i < parseResult.dependencyTokens.length; i++) {
         const dependency = parseResult.dependencyTokens[i];
         if (!dependency.exists) {
@@ -53,12 +59,23 @@ export const getDefinitionSymbolFromMatchMacroAware = async (parseResult: ParseR
             continue;
         }
 
-        const closestSymbol = dependencyParseResult.symbolTable.findClosestSymbol(matchingParseTree.text, undefined, considerMacros);
+        const closestSymbol = dependencyParseResult.symbolTable.findClosestSymbol(matchingParseTree.text, undefined);
         if (!closestSymbol) {
             continue;
         }
 
-        return {uri: dependency, symbol: closestSymbol, origin};
+        currentClosestSymbol = closestSymbol;
+        currentClosestSymbolDependency = dependency;
+        if (closestSymbol.declaration === DeclarationType.ExpandParameter) {
+            // TODO: check in library first for previous declaration, before going to another library
+            continue;
+        }
+
+        break;
+    }
+
+    if (currentClosestSymbol && currentClosestSymbolDependency) {
+        return {uri: currentClosestSymbolDependency, symbol: currentClosestSymbol, origin}
     }
 
     return undefined;

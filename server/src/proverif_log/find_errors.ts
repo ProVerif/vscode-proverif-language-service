eeaaec635e650facd9c379ccb677f95b0cb61308
Florian Moser <git@famoser.ch>
2024-12-01 13:32:53 +0100

    feat: Generalize find errors to false case

--- server/src/proverif_log/find_errors.ts
+++ server/src/proverif_log/find_errors.ts
@@ -3,7 +3,7 @@ import {Diagnostic, Range} from "vscode-languageserver/node";
 import {TextDocument} from "vscode-languageserver-textdocument";
 import {findAllOccurrencesInDocument} from "./scanner";
 
-const UNPROVEN_LEMMA = /(PARTIAL RESULT.+cannot be proved.+)/g;
+const UNPROVEN_LEMMA = /(PARTIAL RESULT.+(cannot be proved|false).+)/g;
 
 export const findFalseLemmas = async (document: TextDocument): Promise<Diagnostic[]> => {
     const results = findAllOccurrencesInDocument(document, UNPROVEN_LEMMA);

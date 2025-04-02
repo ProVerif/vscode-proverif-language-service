0498bd9fd175ab29bbae5df338e8071984c64134
Florian Moser <git@famoser.ch>
2025-04-02 09:30:46 +0200

    fix: Avoid false positive when detecting unproven lemmas

--- server/src/proverif_log/find_errors.ts
+++ server/src/proverif_log/find_errors.ts
@@ -3,7 +3,7 @@ import {Diagnostic} from "vscode-languageserver/node";
 import {TextDocument} from "vscode-languageserver-textdocument";
 import {findAllOccurrencesInDocument} from "./scanner";
 
-const UNPROVEN_LEMMA = /((PARTIAL )?RESULT.+(cannot be proved|false).+)/g;
+const UNPROVEN_LEMMA = /((PARTIAL )?RESULT.+(is false|cannot be proved).+)/g;
 
 export const findFalseLemmas = async (document: TextDocument): Promise<Diagnostic[]> => {
     const results = findAllOccurrencesInDocument(document, UNPROVEN_LEMMA);
